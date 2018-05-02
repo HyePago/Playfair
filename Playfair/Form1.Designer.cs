c001db77160d47ac8698b99d19520bc06c0fbca2
Hyejin Kim <hyepago@naver.com>
2018-05-02 21:02:40 +0900

    #1 Encryption_test

---
+++ Playfair/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Playfair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.keyText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Message = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ChipherText = new System.Windows.Forms.TextBox();
+            this.Encryption = new System.Windows.Forms.Button();
+            this.test = new System.Windows.Forms.Label();
+            this.Decryption = new System.Windows.Forms.Button();
+            this.DecryptionText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // keyText
+            // 
+            this.keyText.Location = new System.Drawing.Point(92, 22);
+            this.keyText.Name = "keyText";
+            this.keyText.Size = new System.Drawing.Size(398, 21);
+            this.keyText.TabIndex = 0;
+            this.keyText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "암호키";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "평문";
+            // 
+            // Message
+            // 
+            this.Message.Location = new System.Drawing.Point(92, 76);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(398, 21);
+            this.Message.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "암호문";
+            // 
+            // ChipherText
+            // 
+            this.ChipherText.Location = new System.Drawing.Point(92, 127);
+            this.ChipherText.Name = "ChipherText";
+            this.ChipherText.Size = new System.Drawing.Size(398, 21);
+            this.ChipherText.TabIndex = 5;
+            // 
+            // Encryption
+            // 
+            this.Encryption.Location = new System.Drawing.Point(33, 222);
+            this.Encryption.Name = "Encryption";
+            this.Encryption.Size = new System.Drawing.Size(75, 23);
+            this.Encryption.TabIndex = 6;
+            this.Encryption.Text = "암호화";
+            this.Encryption.UseVisualStyleBackColor = true;
+            this.Encryption.Click += new System.EventHandler(this.Encryption_Click);
+            // 
+            // test
+            // 
+            this.test.AutoSize = true;
+            this.test.Font = new System.Drawing.Font("굴림", 15F);
+            this.test.Location = new System.Drawing.Point(30, 273);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(57, 20);
+            this.test.TabIndex = 7;
+            this.test.Text = "label4";
+            // 
+            // Decryption
+            // 
+            this.Decryption.Location = new System.Drawing.Point(171, 222);
+            this.Decryption.Name = "Decryption";
+            this.Decryption.Size = new System.Drawing.Size(75, 23);
+            this.Decryption.TabIndex = 9;
+            this.Decryption.Text = "복호화";
+            this.Decryption.UseVisualStyleBackColor = true;
+            // 
+            // DecryptionText
+            // 
+            this.DecryptionText.Location = new System.Drawing.Point(91, 176);
+            this.DecryptionText.Name = "DecryptionText";
+            this.DecryptionText.Size = new System.Drawing.Size(398, 21);
+            this.DecryptionText.TabIndex = 11;
+            this.DecryptionText.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "복호문";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DecryptionText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Decryption);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.Encryption);
+            this.Controls.Add(this.ChipherText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.keyText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox keyText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Message;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ChipherText;
+        private System.Windows.Forms.Button Encryption;
+        private System.Windows.Forms.Label test;
+        private System.Windows.Forms.Button Decryption;
+        private System.Windows.Forms.TextBox DecryptionText;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Playfair/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Playfair
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // 암호화 Button 클릭 시 호출
+        private void Encryption_Click(object sender, EventArgs e)
+        {
+            char[,] playfair = new char[5, 5]; // 암호판 생성
+            int k = 0; // 암호판에 알파벳을 넣을 때 사용할 변수
+            int sw = 0; // Q랑 Z를 한 칸에 넣기 위해 Z일 때 넘기기 위해서 & 전에 넣었던 알파벳을 넣지 않기 위해서 조건을 걸기 위해 사용되는 변수
+
+            string key = keyText.Text.ToLower() ; // 입력받은 암호키를 소문자로 변환하여서 저장
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                if (key[i].Equals(' ')) continue; // 공백이 있으면 그 문자는 넘기고 다음 문자를 입력받음.
+
+                sw = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    // 이 문자가 암호판에 들어가 있는가, 지금 검사하는 것이 'z'인가
+                    if (key[i].Equals(key[j]) || key[i].Equals('z'))
+                    {
+                        sw = 1;
+                        break;
+                    }
+                }
+
+                if (sw == 0) // 검사에서 안 걸린 친구들만 playfiar(암호판)에 넣어준다.
+                {
+                    playfair[k / 5, k % 5] = key[i];
+                    k++;
+                }
+            }
+
+            for (char i = 'a'; i <= 'z'; i++) // 암호키 문자를 넣은 후 'a'부터 중복되지 않는 값을 넣어줌.
+            {
+                sw = 0;
+
+                for (int j = 0; j < k; j++)
+                {
+                    if (playfair[j / 5, j % 5].Equals(i) || i.Equals('z'))
+                    {
+                        sw = 1;
+                        break;
+                    }
+                }
+
+                if (sw == 0)
+                {
+                    playfair[k / 5, k % 5] = i;
+                    k++;
+                }
+            }
+
+            char[,] arr_plain = new char[Message.TextLength, 2]; // 평뮨을 2글자씩 짤라서 넣어줄 배열
+            string plainText = Message.Text;
+            int n = 0, m = 0;
+
+            for (int i = 0; i < Message.TextLength; i++)
+            {
+                if (plainText[i].Equals(' ')) continue; // 중복은 넘어간다.
+                if(i == Message.TextLength-1 && m == 0) // 홀수로 끝나서 한 문자만 남았을 경우
+                {
+                    arr_plain[n, m] = plainText[i];
+                    arr_plain[n, 1] = 'x'; // 마지막 공간을 'x'로 채워줌
+                    n++;
+                    break;
+                }
+                if (m == 0) // 2글자씩 들어가는 곳에서 첫번째 공간에 넣을 때
+                {
+                    arr_plain[n, m] = plainText[i];
+                    m = 1;
+                    continue;
+                }
+
+                if (arr_plain[n, 0].Equals(plainText[i])) // 2글자 씩 들어가는 데 순서대로 들어가면 똑같은 문자가 들어가게 될 경우
+                {
+                    i--; // 이 문자가 안들어가니까 다음 순서에 이 문자를 넣어주기 위하여
+                    arr_plain[n, m] = 'x'; // 'x' 입력
+                    n++;
+                    m = 0;
+                    continue;
+                }
+
+                arr_plain[n, m] = plainText[i]; // 저 조건에 속하지 않을 경우
+                m = 0;
+                n++;
+            }
+
+            char[ , ] chipher_text = new char[n, 2]; // 암호화를 한 문장을 넣을 공간
+            int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+
+            for (int i = 0; i < n; i++) 
+            {
+                for(int j=0; j < 5; j++)
+                {
+                    for(k=0; k<5; k++)
+                    {
+                        if (playfair[j, k].Equals(arr_plain[i, 0])) // 두 글자 중 첫 문자의 위치를 찾는 조건
+                        {
+                            x1 = j;
+                            y1 = k;
+                        }
+                        else if(playfair[j, k].Equals(arr_plain[i, 1])) // 두 글자 중 두번째 문자의 위치를 찾는 조건
+                        {
+                            x2 = j;
+                            y2 = k;
+                        }
+                    }
+                }
+
+                if(y1 == y2) // 같은 열에있으면
+                {
+                    chipher_text[i, 0] = playfair[x1 + 1, y1];
+                    chipher_text[i, 1] = playfair[x2 + 1, y2];
+                    continue;
+                } else if(x1 == x2) // 같은 행에 있으면
+                {
+                    chipher_text[i, 0] = playfair[x1, y1 + 1];
+                    chipher_text[i, 1] = playfair[x2, y2 + 1];
+                    continue;
+                }
+
+                // 둘 다 아니면
+                chipher_text[i, 0] = playfair[x2, y1];
+                chipher_text[i, 1] = playfair[x1, y2];
+            }
+
+            string result = "";
+
+            for(int i=0; i<n; i++)
+            {
+                result += chipher_text[i, 0];
+                result += chipher_text[i, 1];
+            }
+
+            ChipherText.Text = result; // 암호문 출력
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
